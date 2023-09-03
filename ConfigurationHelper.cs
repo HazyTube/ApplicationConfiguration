17a867097a8661870b18926d8724a5fbd07d407f
Zafer A <39789088+HazyTube@users.noreply.github.com>
2023-09-03 13:36:51 +0200

    Initial Commit

---
+++ ConfigurationHelper.cs
@@ -0,0 +1,70 @@
+﻿//#####################################################################################################################
+//
+//                  ApplicationConfiguration
+//                  Library (.dll) used for managing application wide settings/configuration using XML
+//
+//                  Developed by Zafer Altan
+//                  Copyright © 2023 ZA Ecom
+//
+//#####################################################################################################################
+
+namespace ApplicationConfiguration
+{
+    public class ConfigurationHelper
+    {
+
+        private static readonly XmlSettingsManager settingsManager = new XmlSettingsManager();
+
+        private static Dictionary<string, string> _loadedSettings;
+
+        public static Dictionary<string, string> LoadedSettings
+        {
+            get
+            {
+                if (_loadedSettings == null)
+                    _loadedSettings = settingsManager.LoadSettings();
+
+                return _loadedSettings;
+            }
+        }
+
+        // Default settings
+        public static Dictionary<string, string> defaultSettings = new Dictionary<string, string>
+            {
+                { "setting1", "yes" },
+                { "setting2", "no" }
+
+            };
+
+        public static void ResetSettingsToDefault()
+        {
+            _loadedSettings = defaultSettings;
+            settingsManager.SaveSettings(defaultSettings);
+        }
+
+        // Update the value of a single setting key
+        public static void ChangeSetting(string key, string value)
+        {
+            if (LoadedSettings.ContainsKey(key))
+            {
+                LoadedSettings[key] = value;
+                settingsManager.SaveSettings(LoadedSettings);
+            }
+        }
+
+        // Update the value of multiple setting keys, use a dictionary of strings
+        public static void ChangeMultipleSettings(Dictionary<string, string> newSettings)
+        {
+            foreach (var newSetting in newSettings)
+            {
+                if (LoadedSettings.ContainsKey(newSetting.Key))
+                {
+                    LoadedSettings[newSetting.Key] = newSetting.Value;
+                }
+            }
+
+            settingsManager.SaveSettings(LoadedSettings);
+        }
+
+    }
+}
---
+++ XmlSettingsManager.cs
@@ -0,0 +1,96 @@
+﻿//#####################################################################################################################
+//
+//                  ApplicationConfiguration
+//                  Library (.dll) used for managing application wide settings/configuration using XML
+//
+//                  Developed by Zafer Altan
+//                  Copyright © 2023 ZA Ecom
+//
+//#####################################################################################################################
+
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace ApplicationConfiguration
+{
+    public class XmlSettingsManager
+    {
+
+        private readonly string _filePath;
+
+        public XmlSettingsManager()
+        {
+            string executablePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            _filePath = Path.Combine(executablePath, "App.config");
+        }
+
+        public Dictionary<string, string> LoadSettings()
+        {
+            Dictionary<string, string> settings = new Dictionary<string, string>();
+
+            try
+            {
+                if (File.Exists(_filePath))
+                {
+                    XDocument xmlDoc = XDocument.Load(_filePath);
+                    foreach (var element in xmlDoc.Root.Elements())
+                    {
+                        string key = element.Name.ToString();
+                        string value = element.Value;
+                        settings[key] = value;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error sloading settings: " + ex.Message);
+            }
+
+            return settings;
+        }
+
+        public void SaveSettings(Dictionary<string, string> settings)
+        {
+            try
+            {
+                XDocument xmlDoc = new XDocument(new XElement("Settings"));
+
+                foreach (var setting in settings)
+                {
+                    xmlDoc.Root.Add(new XElement(setting.Key, setting.Value));
+                }
+
+                xmlDoc.Save(_filePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error saving settings: " + ex.Message);
+            }
+        }
+
+        public void InitializeDefaultSettings(Dictionary<string, string> defaultSettings)
+        {
+            try
+            {
+                if (!File.Exists(_filePath))
+                {
+                    XDocument xmlDoc = new XDocument(new XElement("Settings"));
+
+                    foreach (var setting in defaultSettings)
+                    {
+                        xmlDoc.Root.Add(new XElement(setting.Key, setting.Value));
+                    }
+
+                    xmlDoc.Save(_filePath);
+                    Console.WriteLine("Default settings file created.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions here
+                Console.WriteLine("Error initializing default settings: " + ex.Message);
+            }
+        }
+
+    }
+}
